35e95bebf6e34cd0ae427a6c068a8b9c61a7a4bb
M <91100579+mary-vo@users.noreply.github.com>
2021-11-13 05:29:14 +0000

    Upload code for Who's paying exercise

--- day4/day-4-2-exercise.py
+++ day4/day-4-2-exercise.py
@@ -46,6 +46,16 @@ You are going to write a program which will select a random name from a list of 
 **Important**: You are not allowed to use the `choice()` function.
 
 **Line 8** splits the string `names_string` into individual names and puts them inside a **List** called `names`. For this to work, you must enter all the names as name followed by comma then space. e.g. name, name, name
+
+# Example Input
+Angela, Ben, Jenny, Michael, Chloe
+
+Note: notice that there is a space between the comma and the next name. 
+
+
+# Example Output
+Michael is going to buy the meal today!
+
 '''
 
 # Split string method
@@ -54,3 +64,15 @@ names = names_string.split(", ")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
+print(names)
+
+count = len(names)
+print(count)
+
+import random
+random_list = random.randint(0,count) 
+print(random_list)
+
+random_person = names[random_list]
+# print(random_person)
+print(f"{random_person} is going to buy the meal today!")
