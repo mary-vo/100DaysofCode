d953d3d664fa3412048b5e6453129afead72d7c6
M <91100579+mary-vo@users.noreply.github.com>
2021-11-13 04:11:46 +0000

    Upload head or tails exercise

--- day4/day-4-1-exercise.py
+++ day4/day-4-1-exercise.py
@@ -1,22 +1,45 @@
 ######## Notes from lecture
+'''
+Random module: https://www.askpython.com/python-modules/python-random-module-generate-random-numbers-sequences
 
+You can import a module to use in your code
 
+import random 
+random_integer = random.randint(1,10)
+print(random_interger)
+
+randon_float = random.random()
+
+random_decimal = random.randon() * 5
+
+love_score = random.randint(1,100)
+print(f"Your love score is {love_score}.")
+'''
 
 '''
-######## Exercise - Odd or Even Exercise
-#Write a program that works out whether if a given number is an odd or even number.
-#Even numbers can be divided by 2 with no remainder.
-#The modulo is written as a percentage sign (%) in Python. It gives you the remainder after a division.
+######## Exercise - Head or Tails
+# Instructions
 
-# 🚨 Don't change the code below 👇
-number = int(input("Which number do you want to check? "))
-# 🚨 Don't change the code above 👆
+You are going to write a virtual coin toss program. It will randomly tell the user "Heads" or "Tails". 
 
+**Important**, the first letter should be capitalised and spelt exactly like in the example e.g. Heads, not heads.
+
+There are many ways of doing this. But to practice what we learnt in the last lesson, you should generate a random number, either 0 or 1. Then use that number to print out Heads or Tails.
+
+e.g.
+1 means Heads
+0 means Tails 
+'''
 #Write your code below this line 👇
-odd_or_even = number % 2
+#Hint: Remember to import the random module first. 🎲
 
-if odd_or_even == 0:
-  print("This is an even number.")
+import random
+
+random_side = random.randint(0,1)
+if random_number == 0:
+  print("Tails")
 else:
-  print("This is an odd number.")
-'''
+  print("Heads")
+
+
+
